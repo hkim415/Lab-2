c5fbfd3c2e96a9f5e5f11c677c8db6278c918260
Hongsoon Kim <khsun6226@naver.com>
2020-09-25 21:35:49 -0700

    add switch case scenario for input month

--- lab-three.js
+++ lab-three.js
@@ -6,6 +6,7 @@ const getDayOfTheWeek = (year, month, day) => {
     let fitTwelves;
     let remainder;
     let fitFours;
+    let convertedMonth;
     let sum;
     let final;
 
@@ -14,10 +15,53 @@ const getDayOfTheWeek = (year, month, day) => {
     remainder = lastTwoDigit % 12;
     fitFours = Math.floor(remainder / 4);
 
+    switch (month.toUpperCase()) {
+        case "JANUARY":
+            convertedMonth = 0;
+            break;
+        case "FEBRUARY":
+            convertedMonth = 1;
+            break;
+        case "MARCH":
+            convertedMonth = 2;
+            break;
+        case "APRIL":
+            convertedMonth = 3;
+            break;
+        case "MAY":
+            convertedMonth = 4;
+            break;
+        case "JUNE":
+            convertedMonth = 5;
+            break;
+        case "JULY":
+            convertedMonth = 6;
+            break;
+        case "AUGUST":
+            convertedMonth = 7;
+            break;
+        case "SEPTEMBER":
+            convertedMonth = 8;
+            break;
+        case "OCTOBER":
+            convertedMonth = 9;
+            break;
+        case "NOVEMBER":
+            convertedMonth = 10;
+            break;
+        case "DECEMBER":
+            convertedMonth = 11;
+            break; 
+    }
+
+    sum = fitTwelves + remainder + fitFours + day + monthCode[convertedMonth];
+
     console.log(lastTwoDigit);
     console.log(fitTwelves);
     console.log(remainder);
     console.log(fitFours);
+    console.log(convertedMonth);
+    console.log(sum);
 }
 
-getDayOfTheWeek(1992, 0,0);
+getDayOfTheWeek(1993, "FEBrUary", 12);
